419db957c46d272e760922c5614fc404a0f70d20
Casey Houle <c.j.houle75@gmail.com>
2025-08-18 14:55:59 -0700

    added knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: "127.0.0.1",
+      password: "secret",
+      user: "postgres",
+      port: 5433,
+      database: "pet_store",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
